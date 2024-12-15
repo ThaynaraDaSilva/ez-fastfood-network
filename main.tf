41aec3e1c68cb3d550a88b4ea9891c1c9ae7c85a
Thaynara Silva <thaynara.silva@yaman.com.br>
2024-12-15 20:13:51 -0300

    provider adicionado direto no main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
+# main.tf
+provider "aws" {
+  region                  = var.region
+  access_key              = var.access_key
+  secret_key              = var.secret_key
+  default_tags {
+    tags = {
+      Project = var.project
+    }
+  }
+}
 
 module "vpc" {
   source      = "./vpc"
